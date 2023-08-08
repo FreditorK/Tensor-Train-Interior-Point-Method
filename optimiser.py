Fixed normalisation of normal part 2

optimiser.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 import numpy as np
 from operators import D_func
 from tt_op import *
@@ -30,7 +32,8 @@ class ILPSolver:
         self.boolean_criterion = tt_boolean_criterion(self.const_space.atom_count)
         self.params = {
             "lr": 0.075,
-            "noise": 0.9 * self.error_bound
+            "noise": 0.9 * self.error_bound,
+            "patience": 5
         }
 
     def _const_satisfied(self):
@@ -93,23 +96,28 @@ class ILPSolver:
         print("\n", flush=True)
 
     def _riemannian_grad(self):
-        criterion = np.inf
+        criterions = deque([np.inf], maxlen=self.params["patience"])
         self._init_hypotheses()
-        while criterion >= 0.1 * self.error_bound * self.params["lr"]:  # Gradient induced change, i.e. similar to first-order sufficient condition
+        while criterions[-1] >= 0.05 * self.error_bound:  # Gradient induced change, i.e. similar to first-order sufficient condition
             hypotheses_copies = deepcopy([h.value for h in self.const_space.hypotheses])
             self._gradient_update()
             self._project()
             criterion = self.const_space.stopping_criterion([h.value for h in self.const_space.hypotheses],
                                                             hypotheses_copies)
+            criterions.append(criterion)
             print(f"Stopping Criterion: {criterion} \r", end="")
+            if (np.array(criterions)[:-1] - np.array(criterions)[1:]).mean() > 0:
+                self.params["lr"] *= 0.99
         print("\n", flush=True)
 
     def _round_solution(self):
         criterion_score = np.mean([self.boolean_criterion(h.value) for h in self.const_space.hypotheses])
+        print(self._const_satisfied())
         while criterion_score > self.error_bound:
             for h in self.const_space.hypotheses:
                 self.const_space.round(h)
                 h.value = tt_rank_retraction([core.shape[-1] for core in h.value[:-1]], h.value)
+                print(self._const_satisfied())
             criterion_score = np.mean([self.boolean_criterion(h.value) for h in self.const_space.hypotheses])
             print(f"Boolean Criterion: {criterion_score} \r", end="")
         print("\n", flush=True)

utils.py
@@ -298,8 +298,9 @@ class LogicConstraint(ABC):
 
     def get_projection(self):
         normal, bias = self._get_hyperplane()
-        normed_normal = tt_mul_scal(1/jnp.sqrt(tt_inner_prod(normal, normal)), normal)
-        return lambda tt_h: self._projection(tt_h, normed_normal, bias)
+        if abs(bias) > 1:  # TODO: Then it is unsatisfiable, only happens in Multi-Hypothesis case
+            return lambda tt_h: (tt_h, False)
+        return lambda tt_h: self._projection(tt_h, normal, bias)
 
     @abstractmethod
     def is_satisfied(self, tt_h):
@@ -313,8 +314,9 @@ class ExistentialConstraint(LogicConstraint):
         func_result = tt_inner_prod(tt_h, tt_n)
         condition = func_result + bias <= self.s_lower
         if condition:
-            alpha = np.sqrt((1-(bias- self.s_lower)**2)/(1 - func_result**2))
-            beta = bias - self.s_lower + alpha*func_result
+            tt_n = tt_mul_scal(1 / jnp.sqrt(tt_inner_prod(tt_n, tt_n)), tt_n)
+            alpha = np.sqrt((1 - (bias - self.s_lower) ** 2) / (1 - func_result ** 2))
+            beta = bias - self.s_lower + alpha * func_result
             tt_h = tt_add(tt_mul_scal(alpha, tt_h), tt_mul_scal(-beta, tt_n))
             tt_h = tt_rank_reduce(tt_h)
         return tt_h, condition
@@ -331,8 +333,9 @@ class UniversalConstraint(LogicConstraint):
         func_result = tt_inner_prod(tt_h, tt_n)
         condition = abs(func_result + bias) >= self.s_lower
         if condition:
-            alpha = np.sqrt((1-bias**2)/(1 - func_result**2))
-            beta = bias + alpha*func_result
+            tt_n = tt_mul_scal(1 / jnp.sqrt(tt_inner_prod(tt_n, tt_n)), tt_n)
+            alpha = np.sqrt((1 - bias ** 2) / (1 - func_result ** 2))
+            beta = bias + alpha * func_result
             tt_h = tt_add(tt_mul_scal(alpha, tt_h), tt_mul_scal(-beta, tt_n))
             tt_h = tt_rank_reduce(tt_h)
         return tt_h, condition
@@ -416,7 +419,7 @@ class ConstraintSpace(ParameterSpace, ABC):
         not_converged = True
         while not_converged:
             not_converged = False
-            for proj in np.random.permutation(projections):
+            for proj in projections:
                 proj_tt_train, is_violated = proj(proj_tt_train)
                 not_converged = not_converged or is_violated
         hypothesis.value = proj_tt_train