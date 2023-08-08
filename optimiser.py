Fixed normal normalisation

optimiser.py
@@ -30,8 +30,18 @@ class ILPSolver:
         self.boolean_criterion = tt_boolean_criterion(self.const_space.atom_count)
         self.params = {
             "lr": 0.08,
-            "noise": 0.9*self.error_bound
+            "noise": 0.9 * self.error_bound
         }
+
+    def _const_satisfied(self):
+        is_satisfied = True
+        for h in self.const_space.hypotheses:
+            for c in self.const_space.eq_constraints[h]:
+                is_satisfied = is_satisfied & c.is_satisfied(h.value)
+                print(c, c.is_satisfied(h.value))
+            for c in self.const_space.iq_constraints[h]:
+                is_satisfied = is_satisfied & c.is_satisfied(h.value)
+        return is_satisfied
 
     def solve(self, timeout=100):
         iter_function = self._project if self.objective_grad is None else self._riemannian_grad
@@ -39,10 +49,9 @@ class ILPSolver:
             print(f"----------Iteration {i}----------")
             iter_function()
             self._round_solution()
-            #criterion_score = self.const_space.stopping_criterion() TODO
-            if True: #criterion_score < self.error_bound:
+            self._const_satisfied()
+            if self._const_satisfied():
                 break
-            print(f"Constraint Criterion after rounding: {criterion_score}")
             self._stir_up()
 
     def _stir_up(self):
@@ -51,15 +60,16 @@ class ILPSolver:
             h.value = tt_add_noise(h.value, rank=rank, noise_radius=self.error_bound)
 
     def _gradient_update(self):
-        tt_trains = [tt_add_noise(h.value, rank=1, noise_radius=self.params["noise"]) for h in self.const_space.hypotheses]
+        tt_trains = [tt_add_noise(h.value, rank=1, noise_radius=self.params["noise"]) for h in
+                     self.const_space.hypotheses]
         for idx in range(self.const_space.atom_count):
             gradients = self.objective_grad(tt_trains)
-            for i, tt_train in enumerate(tt_trains):
+            for i in range(self.const_space.hypothesis_count):
                 gradient = gradients[i][idx]
-                tt_train[idx] -= self.params["lr"] * gradient
-                tt_train[idx] += self.params["lr"] * tt_grad_inner_prod(tt_train, tt_train, gradient, idx) * \
-                             tt_train[idx]
-                tt_train = tt_normalise(tt_train, idx=idx)
+                tt_trains[i][idx] -= self.params["lr"] * gradient
+                tt_trains[i][idx] += self.params["lr"] * tt_grad_inner_prod(tt_trains[i], tt_trains[i], gradient, idx) * \
+                                 tt_trains[i][idx]
+                tt_trains[i] = tt_normalise(tt_trains[i], idx=idx)
         for h, tt_train in zip(self.const_space.hypotheses, tt_trains):
             h.value = tt_train
 
@@ -70,10 +80,11 @@ class ILPSolver:
     def _init_hypotheses(self):
         print("Initialising hypotheses...")
         criterion = np.inf
-        while criterion >= 0.1*self.error_bound * self.params["lr"]:
+        while criterion >= 0.1 * self.error_bound * self.params["lr"]:
             hypotheses_copies = deepcopy([h.value for h in self.const_space.hypotheses])
             self._gradient_update()
-            criterion = self.const_space.stopping_criterion([h.value for h in self.const_space.hypotheses], hypotheses_copies)
+            criterion = self.const_space.stopping_criterion([h.value for h in self.const_space.hypotheses],
+                                                            hypotheses_copies)
             print(f"Stopping Criterion: {criterion} \r", end="")
         print("\n", flush=True)
 
@@ -81,11 +92,13 @@ class ILPSolver:
         criterion = np.inf
         self._init_hypotheses()
         print("Optimising the relaxation...")
-        while criterion >= 0.1*self.error_bound*self.params["lr"]:  # Gradient induced change, i.e. similar to first-order sufficient condition
+        while criterion >= 0.1 * self.error_bound * self.params[
+            "lr"]:  # Gradient induced change, i.e. similar to first-order sufficient condition
             hypotheses_copies = deepcopy([h.value for h in self.const_space.hypotheses])
             self._gradient_update()
             self._project()
-            criterion = self.const_space.stopping_criterion([h.value for h in self.const_space.hypotheses], hypotheses_copies)
+            criterion = self.const_space.stopping_criterion([h.value for h in self.const_space.hypotheses],
+                                                            hypotheses_copies)
             print(f"Stopping Criterion: {criterion} \r", end="")
         print("\n", flush=True)
 

tt_op.py
@@ -150,7 +150,7 @@ def tt_rank_reduce(tt_train: List[np.array]):
 
 
 def tt_rank(tt_train):
-    return np.max([max(t.shape[0], t.shape[1]) for t in tt_train])
+    return np.max([max(t.shape[0], t.shape[-1]) for t in tt_train])
 
 
 def tt_rank_retraction(tt_upper_ranks: List[int], tt_train: List[np.array]):
@@ -521,8 +521,8 @@ def tt_neg(tt_train: List[np.array]) -> List[np.array]:
 
 
 def tt_mul_scal(alpha, tt_train, idx=0):
-    tt_train[idx] *= alpha
-    return tt_train
+    mul = alpha*tt_train[idx]
+    return tt_train[:idx] + [mul] + tt_train[idx+1:]
 
 
 def tt_normalise(tt_train, radius=1, idx=0):

utils.py
@@ -290,7 +290,6 @@ class LogicConstraint(ABC):
         bias = tt_leading_entry(tt_expr.cores) - self.percent
         last_normal_core = np.einsum("ldr, rk -> ldk", tt_expr.cores[-2], tt_expr.cores[-1][:, 1, :])
         normal = tt_expr.cores[:-2] + [last_normal_core]
-        normal = tt_mul_scal(1/jnp.sqrt(tt_inner_prod(normal, normal)), normal)
         return normal, bias
 
     @abstractmethod
@@ -299,33 +298,48 @@ class LogicConstraint(ABC):
 
     def get_projection(self):
         normal, bias = self._get_hyperplane()
-        return lambda tt_h: self._projection(tt_h, normal, bias)
+        normed_normal = tt_mul_scal(1/jnp.sqrt(tt_inner_prod(normal, normal)), normal)
+        return lambda tt_h: self._projection(tt_h, normed_normal, bias)
+
+    @abstractmethod
+    def is_satisfied(self, tt_h):
+        ...
 
 
 class ExistentialConstraint(LogicConstraint):
 
     def _projection(self, tt_h, tt_n, bias):
+        tt_n = deepcopy(tt_n)
         func_result = tt_inner_prod(tt_h, tt_n)
         condition = func_result + bias <= self.s_lower
         if condition:
             alpha = np.sqrt((1-(bias- self.s_lower)**2)/(1 - func_result**2))
             beta = bias - self.s_lower + alpha*func_result
-            tt_h = tt_add(tt_mul_scal(alpha, tt_h), tt_mul_scal(-beta, deepcopy(tt_n)))
+            tt_h = tt_add(tt_mul_scal(alpha, tt_h), tt_mul_scal(-beta, tt_n))
             tt_h = tt_rank_reduce(tt_h)
         return tt_h, condition
+
+    def is_satisfied(self, tt_h):
+        normal, bias = self._get_hyperplane()
+        return tt_inner_prod(tt_h, normal) + bias > self.s_lower
 
 
 class UniversalConstraint(LogicConstraint):
 
     def _projection(self, tt_h, tt_n, bias):
+        tt_n = deepcopy(tt_n)
         func_result = tt_inner_prod(tt_h, tt_n)
         condition = abs(func_result + bias) >= self.s_lower
         if condition:
             alpha = np.sqrt((1-bias**2)/(1 - func_result**2))
             beta = bias + alpha*func_result
-            tt_h = tt_add(tt_mul_scal(alpha, tt_h), tt_mul_scal(-beta, deepcopy(tt_n)))
+            tt_h = tt_add(tt_mul_scal(alpha, tt_h), tt_mul_scal(-beta, tt_n))
             tt_h = tt_rank_reduce(tt_h)
         return tt_h, condition
+
+    def is_satisfied(self, tt_h):
+        normal, bias = self._get_hyperplane()
+        return abs(tt_inner_prod(tt_h, normal) + bias) < self.s_lower
 
 
 class ConstraintSpace(ParameterSpace, ABC):