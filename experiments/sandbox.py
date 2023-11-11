80ddd446036dfa73c781e9c340dd388f8ab426a1
FreddoIsHere <fkelbel@ymail.com>
2023-11-11 16:31:48 +0000

    Backup: Normalised inner product

--- experiments/sandbox.py
+++ experiments/sandbox.py
@@ -11,36 +11,40 @@ from itertools import product
 from src.tt_op import *
 
 
-def _tt_core_collapse(core_1: np.array, core_2: np.array):
-    return sum([
-        np.kron(core_1[(slice(None),) + i], core_2[(slice(None),) + i])
-        for i in product(*([[0, 1]] * (len(core_1.shape) - 2)))
-    ])
+def proj(tt_h, tt_n, func_result, bias):
+    tt_n = tt_normalise(tt_n)
+    alpha = jnp.sqrt((1 - bias ** 2) / (1 - func_result ** 2))
+    beta = bias + alpha * func_result
+    tt_h = tt_add(tt_mul_scal(alpha, tt_h), tt_mul_scal(-beta, tt_n))
+    return tt_h
 
 
-def matrix_normalise(m):
-    g = np.random.randn(m.shape[0], m.shape[0])
-
-    return (1/np.linalg.norm(g))*g @ m
-
-
-def tt_inner_prod_norm(tt_train_1: List[np.array], tt_train_2: List[np.array]) -> float:
-    """
-    Computes the inner product between two tensor trains
-    """
-    return jnp.sum(
-        jnp.linalg.multi_dot(
-            [matrix_normalise(_tt_core_collapse(core_1, core_2)) for core_1, core_2 in zip(tt_train_1, tt_train_2)])
-    )
-
-
-target_ranks = [1, 2, 3, 2, 2, 5, 6, 1]
-noise_train = tt_rl_orthogonalize(tt_normalise(
+target_ranks = [1, 3, 3, 2, 3, 3, 5, 3, 3, 3, 7, 3, 1]
+noise_train_1 = tt_rl_orthogonalize(tt_normalise(
     [(1 / (l_n * 2 * l_np1)) * np.random.randn(l_n, 2, l_np1) for l_n, l_np1 in
      zip(target_ranks[:-1], target_ranks[1:])]))
-noise_train_2 = tt_add_noise(noise_train, target_ranks[1:-1])
-noise_train_3 = tt_rl_orthogonalize(tt_normalise(
+
+target_ranks = [1, 3, 3, 2, 3, 3, 5, 3, 3, 3, 7, 3, 1]
+noise_train_2 = tt_rl_orthogonalize(tt_normalise(
     [(1 / (l_n * 2 * l_np1)) * np.random.randn(l_n, 2, l_np1) for l_n, l_np1 in
      zip(target_ranks[:-1], target_ranks[1:])]))
-print(tt_inner_prod(noise_train, noise_train_2), tt_inner_prod(noise_train, noise_train_3))
-print(tt_inner_prod_norm(noise_train, noise_train_2), tt_inner_prod_norm(noise_train_2, noise_train_3))
+
+print(len(noise_train_2))
+func_result = tt_inner_prod(noise_train_1, noise_train_2)
+print(func_result)
+noise_train_1_projection = tt_rl_orthogonalize(proj(noise_train_1, noise_train_2, func_result, 0))
+print(tt_inner_prod(noise_train_1_projection, noise_train_2))
+print(tt_normed_inner_prod(noise_train_1_projection, noise_train_2))
+"""
+t = [0]*len(noise_train_1_projection)
+for _ in range(100):
+    ks = tt_randomised_inner_prod_2(noise_train_1_projection, noise_train_1_projection)
+    t = [a + k for a, k in zip(t, ks)]
+print([a/1000 for a in t])
+
+m = 0
+for _ in range(10000):
+    vec = np.random.randn(3, 1)
+    m += vec @ vec.T
+print(m/10000)
+"""
