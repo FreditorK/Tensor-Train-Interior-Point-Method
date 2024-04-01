193bd24f7438f5485a6c0bbb09638effd97bb0a8
FreddoIsHere <fkelbel@ymail.com>
2024-04-01 18:38:08 +0100

    partial success on theta estimation

--- experiments/sandbox_2.py
+++ experiments/sandbox_2.py
@@ -16,39 +16,57 @@ def sweep_to_lower_range():
     pass
 
 A = 0.2*np.array([[1, 2, 1], [-4, 1, -1]])
-B = 0.08*np.array([[2, 1], [-5, -1], [3, 2]])
+B = -0.08*np.array([[2, 1], [-5, -1], [3, 2]])
 C = 0.1*np.array([[-1, 4], [5.3, 2]])
 result = A @ B @ C
 print("Actual result: ", np.sum(np.diagonal(result)))
 
-"""
+#A = np.block([[-A, np.zeros((A.shape[0], 1))], [np.zeros((1, A.shape[1])), theta]])
+#B = np.block([[B, np.zeros((B.shape[0], 1))], [np.zeros((1, B.shape[1])), theta]])
+#C = np.block([[C, np.zeros((C.shape[0], 1))], [np.zeros((1, C.shape[1])), theta]])
+
 m = 0
 factors = []
-K = 100
+K = 200
 for _ in range(K):
     vec_1 = 2*np.round(np.random.rand(A.shape[0], 1))-1
     vec_2 = 2*np.round(np.random.rand(B.shape[0], 1))-1
     vec_3 = 2*np.round(np.random.rand(C.shape[0], 1))-1
-    # TODO: We could rescale the random vectors through the sampling to equalise the factors
-    #vec_4 = np.random.randn(1, 2)
     a = (vec_1.T @ (A) @ vec_2).item()
     b = (vec_2.T @ (B) @ vec_3).item()
     c = (vec_3.T @ (C) @ vec_1).item()
     factors.append(np.array([a, b, c]))
-    m += a*b*c
 
-# Rademacher is a better estimator
-print("Approximated result: ", m/K)
+
+theta = 1
 factors = np.array(factors)
-factor_sgns = np.prod(np.sign(factors), axis=1)
-abs_factors = np.abs(factors)
+sign_count = K
+iteration = 1
+step = 0.5
+while K/2 -1 > sign_count or sign_count > K/2 + 1 :
+    sign_list = []
+    for i in range(K):
+        a = np.sign(theta - factors[i, 0])
+        b = np.sign(theta + factors[i, 1])
+        c = np.sign(theta + factors[i, 2])
+        sign_list.append(np.array([a*b*c]))
 
-norm = K**(1/3)
-A = np.diag(factors[:, 0])/norm
-B = np.diag(factors[:, 1])/norm
-C = np.diag(factors[:, 2])/norm
+    sign_list = np.array(sign_list)
+    sign_count = len(sign_list[sign_list > 0])
+    if sign_count > K/2:
+        print(f"Count {iteration}: {sign_count}, theta: {theta}")
+        theta -= step
+    elif sign_count < K/2:
+        print(f"Count {iteration}: {sign_count}, theta: {theta}")
+        theta += step
+    if step == 0:
+        break
+    iteration += 1
+    step *= 0.5
+print(f"Count: {sign_count}, theta: {theta}, theta^3: {theta ** 3}")
+
+
 """
-
 G_1 = 2*np.round(np.random.rand(3, 1)) - 1
 G_2 = 2*np.round(np.random.rand(2, 1)) - 1
 G_3 = 2*np.round(np.random.rand(2, 1)) - 1
@@ -70,12 +88,9 @@ Down_C = QCY.T @ RB @ C
 print(np.trace(QAY @ QA @ QBY @ QB @ QCY @ Down_C))
 print(np.trace(QAY @ Down_C))
 print(QAY)
-print(QBY)
-print(QCY)
 print(Down_C)
 
 
-"""
 
 # Create subplots
 fig, axs = plt.subplots(4, 1, figsize=(8, 12))
@@ -89,13 +104,6 @@ for i in range(3):
     axs[i].set_xlabel('Value')
     axs[i].set_ylabel('Frequency')
     axs[i].set_title(f'Histogram of Row {i+1}')
-
-axs[3].hist(prod_factors, bins=500, color='skyblue', edgecolor='black')
-
-# Add labels and title
-axs[3].set_xlabel('Value')
-axs[3].set_ylabel('Frequency')
-axs[3].set_title(f'Prod Factors')
 
 # Adjust layout
 plt.tight_layout()
--- experiments/sandbox_4.py
+++ experiments/sandbox_4.py
@@ -5,43 +5,19 @@ sys.path.append(os.getcwd() + '/../')
 import numpy as np
 
 
-A = 0.5*np.array([[-3, 0], [0, 1]])
-B = 0.2*np.array([[1, 2], [4, -1]])
-C = 0.5*np.array([[2, 0], [0, -0.1]])
+A = np.array([[-0.3, 0], [-0.2, 1]])
+B = np.array([[1, 0.2], [0.4, -0.1]])
+C = np.array([[0.2, 0.5], [0, -0.1]])
 
-print("Truth: ", np.sum(A @ B @ C))
+print("Truth: ", np.trace(A @ B @ C))
 l = 1
 
-theta = (0.135)**(1/3)
-A = np.block([[theta, np.zeros((1, A.shape[1]))], [np.zeros((A.shape[0], 1)), -A]])
-B = np.block([[theta, np.zeros((1, B.shape[1]))], [np.zeros((B.shape[0], 1)), B]])
-C = np.block([[theta, np.zeros((1, C.shape[1]))], [np.zeros((C.shape[0], 1)), C]])
+theta = (0.54)**(1/3)
+A = np.block([[-A, np.zeros((A.shape[0], 1))], [np.zeros((1, A.shape[1])), theta]])
+B = np.block([[B, np.zeros((B.shape[0], 1))], [np.zeros((1, B.shape[1])), theta]])
+C = np.block([[C, np.zeros((C.shape[0], 1))], [np.zeros((1, C.shape[1])), theta]])
 
-a = 0
-k = 1
-for _ in range(k):
-    r_1 = np.random.randn(3, l)
-    r_2 = np.random.randn(3, l)
-    r_3 = np.random.randn(3, l)
-    r_4 = np.random.randn(3, l)
-    AY = A @ r_1
-    QAY, _ = np.linalg.qr(AY)
-    Down_A = QAY.T @ A
-    QA, RA = np.linalg.qr(Down_A)
-
-    B_p = RA @ B
-    BY = B_p @ r_2
-    QBY, _ = np.linalg.qr(BY)
-    Down_B = QBY.T @ B_p
-    QB, RB = np.linalg.qr(Down_B)
-
-    C_p = RB @ C
-    CY = C_p @ r_3
-    QCY, _ = np.linalg.qr(CY)
-    Down_C = QCY.T @ C_p
-    QC, RC = np.linalg.qr(Down_C)
-    a += np.sum(RC @ r_4)
-
-
-print("Approximation: ", a/k)
+print(np.trace(A @ B @ C))
+d = np.linalg.eigvals(A @ B @ C)
+print(d)
 
