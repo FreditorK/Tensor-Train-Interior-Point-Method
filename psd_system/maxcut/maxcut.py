6bb44e5c9363fd6804d09f5945994138fdc6a0e1
FreddoIsHere <fkelbel@ymail.com>
2024-10-25 11:45:23 +0100

    Correction maxcut new lagrangian multipliers

--- psd_system/maxcut/maxcut.py
+++ psd_system/maxcut/maxcut.py
@@ -22,13 +22,14 @@ if __name__ == "__main__":
     print("Creating Problem...")
     G_tt = tt_rank_reduce(tt_random_graph(Config.ranks)) # [np.array([[-1, 1], [1, -1]]).reshape(1, 2, 2, 1)] + [np.ones((1, 2, 2, 1)) for _ in Config.ranks]#
     print(np.round(tt_matrix_to_matrix(G_tt), decimals=2))
-    As_tt = tt_mask_to_linear_op(tt_identity(len(G_tt)))
+    diag_tt_op = tt_mask_to_linear_op(tt_identity(len(G_tt)))
+    diag_tt_op_adjoint = tt_mask_to_linear_op_adjoint(tt_identity(len(G_tt)))
     bias_tt = tt_identity(len(G_tt))
     print("...Problem created!")
     print(f"Objective Ranks: {tt_ranks(G_tt)}")
-    print(f"Constraint Ranks: As {tt_ranks(As_tt)}, bias {tt_ranks(bias_tt)}")
+    print(f"Constraint Ranks: As {tt_ranks(diag_tt_op)}, bias {tt_ranks(bias_tt)}")
     t0 = time.time()
-    X_tt, Y_tt, Z_tt = tt_ipm(G_tt, As_tt, bias_tt, verbose=True)
+    X_tt, Y_tt, Z_tt = tt_ipm(G_tt, diag_tt_op, diag_tt_op_adjoint, bias_tt, verbose=True)
     t1 = time.time()
     print("Solution: ")
     np.set_printoptions(linewidth=600, threshold=np.inf, precision=4, suppress=True)
@@ -37,12 +38,3 @@ if __name__ == "__main__":
     print("Complementary Slackness: ", tt_inner_prod(X_tt, Z_tt))
     print(f"Ranks X_tt: {tt_ranks(X_tt)}, Y_tt: {tt_ranks(Y_tt)}, Z_tt: {tt_ranks(Z_tt)} ")
     print(f"Time: {t1-t0}s")
-    """
-    print(f"Problem solved in {t1 - t0:.3f}s")
-    print(f"Objective value: {-tt_inner_prod(G, X)}")
-    evaled_constraints = tt_linear_op(As, X)
-    scaled_error = [c / np.sqrt(c.shape[1]) for c in tt_add(evaled_constraints, tt_scale(-1, bias))]
-    avg_error = np.sqrt(tt_inner_prod(scaled_error, scaled_error))
-    print(f"Avg constraint error: {avg_error}")
-    print("Ranks of X: ", tt_ranks(X))
-    """
