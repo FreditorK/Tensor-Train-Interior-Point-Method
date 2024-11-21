45ae8634d7ad3c17145faecf50fe7a0e59eea8e2
FreddoIsHere <fkelbel@ymail.com>
2024-11-21 22:23:54 +0000

    Another block amen backup

--- linear_system/block_amen_test.py
+++ linear_system/block_amen_test.py
@@ -11,12 +11,12 @@ from src.tt_amen import tt_amen, tt_block_amen
 np.set_printoptions(linewidth=np.inf, threshold=np.inf, precision=4, suppress=True)
 np.random.seed(4)
 ranks = [2]
-block_1_tt = tt_rank_reduce(tt_random_gaussian(ranks, shape=(2, 2)))
-block_2_tt = tt_rank_reduce(tt_random_gaussian(ranks, shape=(2, 2)))
+block_1_tt = tt_rank_reduce(tt_random_gaussian([4, 2], shape=(2, 2)))
+block_2_tt = tt_rank_reduce(tt_random_gaussian([2, 3], shape=(2, 2)))
 block_matrix_tt = {(0, 0): block_1_tt, (1, 1): block_2_tt}
 
-b_1_tt = tt_rank_reduce(tt_matrix_vec_mul(block_1_tt, tt_random_gaussian(ranks, shape=(2,))))
-b_2_tt = tt_rank_reduce(tt_matrix_vec_mul(block_2_tt, tt_random_gaussian(ranks, shape=(2,))))
+b_1_tt = tt_rank_reduce(tt_matrix_vec_mul(block_1_tt, tt_random_gaussian([1, 2], shape=(2,))))
+b_2_tt = tt_rank_reduce(tt_matrix_vec_mul(block_2_tt, tt_random_gaussian([3, 1], shape=(2,))))
 block_b_tt = {0: b_1_tt, 1: b_2_tt}
 sol, _ = tt_block_amen(block_matrix_tt, block_b_tt, kickrank=2, verbose=True)
 block_sol_1 = sol[:-1] + [sol[-1][:, 0]]
