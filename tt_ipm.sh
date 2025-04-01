9886647ea83833db86229922119293e1c0a4213d
Frederik Kelbel <fkelbel@ymail.com>
2025-04-01 15:02:28 +0100

    -

--- tt_ipm.sh
+++ tt_ipm.sh
@@ -11,21 +11,21 @@ conda activate ttipm
 cd TT-IPM/psd_system/maxcut
 
 echo "Maxcut (TT-IPM) dim=6:"
-timeout 15m python maxcut.py --config configs/maxcut_6.yaml --track_mem
+python maxcut.py --config configs/maxcut_6.yaml --track_mem
 
 echo "Maxcut (Sketch-CGAL) dim=6:"
-timeout 15m python maxcut_sketch_cgal.py --config configs/maxcut_6.yaml --track_mem
+timeout 30m python maxcut_sketch_cgal.py --config configs/maxcut_6.yaml --track_mem
 
 echo "Maxcut (SDPA) dim=6:"
-timeout 15m python maxcut_sdpa.py --config configs/maxcut_6.yaml --track_mem
+python maxcut_sdpa.py --config configs/maxcut_6.yaml --track_mem
 
 cd ../stable_set
 
 echo "MaxStableSet (TT-IPM) dim=6:"
-timeout 15m python max_stable_set.py --config configs/max_stable_set_6.yaml --track_mem
+python max_stable_set.py --config configs/max_stable_set_6.yaml --track_mem
 
 echo "MaxStableSet (Sketch-CGAL) dim=6:"
-timeout 15m python max_stable_set_sketch_cgal.py --config configs/max_stable_set_6.yaml --track_mem
+timeout 30m python max_stable_set_sketch_cgal.py --config configs/max_stable_set_6.yaml --track_mem
 
 echo "MaxStableSet (SDPA) dim=6:"
-timeout 15m python max_stable_set_sdpa.py --config configs/max_stable_set_6.yaml --track_mem
+python max_stable_set_sdpa.py --config configs/max_stable_set_6.yaml --track_mem
