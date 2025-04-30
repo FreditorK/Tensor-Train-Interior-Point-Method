Scaling fixes

psd_system/graphm/graphm.py
@@ -110,6 +110,21 @@ def tt_padding_op(dim):
 
 # ------------------------------------------------------------------------------
 
+
+def tt_obj_matrix(rank, dim):
+    G_A = tt_random_graph(dim, rank)
+    # print("Graph A: ")
+    # print(np.round(tt_matrix_to_matrix(G_A), decimals=2))
+
+    G_B = tt_random_graph(dim, rank)
+    # print("Graph B: ")
+    # print(np.round(tt_matrix_to_matrix(G_B), decimals=2))
+
+    # print("Objective matrix: ")
+    C_tt = [E(0, 0)] + G_B + G_A
+    # print(np.round(tt_matrix_to_matrix(C_tt), decimals=2))
+    return tt_normalise(C_tt, radius=2**(dim/2))
+
 """
         [Q   P  0 ]
     X = [P^T 1  0 ]
@@ -144,17 +159,8 @@ def tt_padding_op(dim):
 
 def create_problem(n, max_rank):
     print("Creating Problem...")
-    G_A = tt_random_graph(n, max_rank)
-    #print("Graph A: ")
-    #print(np.round(tt_matrix_to_matrix(G_A), decimals=2))
 
-    G_B = tt_random_graph(n, max_rank)
-    #print("Graph B: ")
-    #print(np.round(tt_matrix_to_matrix(G_B), decimals=2))
-
-    #print("Objective matrix: ")
-    C_tt = [E(0, 0)] + G_B + G_A
-    #print(np.round(tt_matrix_to_matrix(C_tt), decimals=2))
+    C_tt = tt_obj_matrix(max_rank, n)
 
     test_matrix = tt_random_gaussian([2]*(n+1), (2, 2))
     test_matrix_t = tt_transpose(test_matrix)
@@ -199,7 +205,7 @@ def create_problem(n, max_rank):
 
     # ---
     # IX
-    kappa = 0.0 # Relaxing the psd  condition a little bit to come closer to boundary
+    kappa = 1e-3 # Relaxing the psd  condition a little bit to come closer to boundary
     padding_op = tt_reshape(tt_padding_op(2 * n), (4, 4))
     padding_op_bias = [(1+kappa)*E(1, 1)] + tt_identity(2 * n)
 
@@ -262,7 +268,6 @@ if __name__ == "__main__":
         np.random.seed(seed)
         t0 = time.time()
         rank = config["max_rank"] if args.rank == 0 else args.rank
-        G_tt = tt_rank_reduce(tt_random_graph(config["dim"], rank))
         t1 = time.time()
         C_tt, L_op_tt, eq_bias_tt, ineq_mask, lag_maps = create_problem(config["dim"], config["max_rank"])
         lag_maps = {key: tt_reshape(value, (4, 4)) for key, value in lag_maps.items()}
@@ -311,6 +316,12 @@ if __name__ == "__main__":
     print(f"Complementary Slackness avg: {np.mean(complementary_slackness)}")
     print(f"Total feasibility error avg: {np.mean(feasibility_errors)}")
     print("Solution: ")
-    X_tt = tt_rank_reduce(X_tt, 1e-2)
-    print(tt_ranks(X_tt))
     print(np.round(tt_matrix_to_matrix(X_tt), decimals=2))
+    #print(tt_inner_prod(tt_reshape(C_tt, (2, 2)), X_tt))
+    #dual_feas = tt_rank_reduce(tt_sub(tt_fast_matrix_vec_mul(tt_transpose(L_op_tt), Y_tt, 1e-10),
+                                     # tt_rank_reduce(tt_add(tt_reshape(Z_tt, (4,)), C_tt), 1e-10)), 1e-5)
+    #dual_feas = tt_rank_reduce(tt_sub(dual_feas, tt_reshape(T_tt, (4,))), 1e-5)
+    #print(np.round(tt_matrix_to_matrix(tt_reshape(dual_feas, (2, 2))), decimals=2))
+    #n = config["dim"]
+    #op = tt_reshape(tt_partial_J_trace_op(n, 2 * n), (4, 4))
+    #print(tt_matrix_to_matrix(tt_reshape(tt_fast_matrix_vec_mul(op, tt_reshape(X_tt, (4,))), (2, 2))))

psd_system/maxcut/maxcut.py
@@ -20,6 +20,11 @@ def tt_diag_op_adj(dim):
     return tt_diag_op(dim)
 
 
+def tt_obj_matrix(rank, dim):
+    graph = tt_rank_reduce(tt_random_graph(dim, rank))
+    return tt_normalise(graph, radius=2**(dim/2))
+
+
 if __name__ == "__main__":
     np.set_printoptions(linewidth=np.inf, threshold=np.inf, precision=4, suppress=True)
     parser = argparse.ArgumentParser(description="Script with optional memory tracking.")
@@ -41,7 +46,7 @@ if __name__ == "__main__":
         np.random.seed(seed)
         t0 = time.time()
         rank = config["max_rank"] if args.rank == 0 else args.rank
-        G_tt = tt_rank_reduce(tt_random_graph(config["dim"], rank))
+        G_tt = tt_obj_matrix(rank, config["dim"])
         t1 = time.time()
         L_tt = tt_diag_op(config["dim"])
         bias_tt = tt_identity(config["dim"])
@@ -65,7 +70,8 @@ if __name__ == "__main__":
             verbose=config["verbose"],
             feasibility_tol=config["feasibility_tol"],
             centrality_tol=config["centrality_tol"],
-            op_tol=config["op_tol"]
+            op_tol=config["op_tol"],
+            aho_direction=False
         )
         t3 = time.time()
         if args.track_mem:

psd_system/stable_set/max_stable_set.py
@@ -32,6 +32,9 @@ def tt_tr_op(dim):
         op.append(core)
     return tt_rank_reduce(op)
 
+def tt_obj_matrix(dim):
+    return [0.5*np.ones((1, 2, 2, 1)) for _ in range(dim)]
+
 if __name__ == "__main__":
     np.set_printoptions(linewidth=np.inf, threshold=np.inf, precision=4, suppress=True)
     parser = argparse.ArgumentParser(description="Script with optional memory tracking.")
@@ -57,7 +60,7 @@ if __name__ == "__main__":
             G_entry_tt_op = tt_G_entrywise_mask_op(G)
             tr_tt_op = tt_tr_op(config["dim"])
             tr_bias_tt = [E(0, 0) for _ in range(config["dim"])]
-            J_tt = tt_one_matrix(config["dim"])
+            J_tt = tt_obj_matrix(config["dim"])
             lag_maps = {
                 "y": tt_rank_reduce(tt_diag(tt_split_bonds(tt_sub(tt_one_matrix(config["dim"]), tt_add(G, tr_bias_tt)))))
             }

src/tt_ipm.py
@@ -120,7 +120,7 @@ def _ipm_local_solver_ineq(XAX_k, block_A_k, XAX_k1, Xb_k, block_b_k, Xb_k1, pre
         try:
             L_L_Z = scip.linalg.cholesky(
                 cached_einsum('lsr,smnS,LSR->lmLrnR', XAX_k[(2, 1)], block_A_k[(2, 1)], XAX_k1[(2, 1)]).reshape(m, m),
-                check_finite=False, lower=True
+                check_finite=False, lower=True, overwrite_a=True
             )
             mR_p = rhs[:, 0].reshape(m, 1)
             mR_d = rhs[:, 1].reshape(m, 1)
@@ -129,16 +129,10 @@ def _ipm_local_solver_ineq(XAX_k, block_A_k, XAX_k1, Xb_k, block_b_k, Xb_k1, pre
             L_Z_inv_L_X = forward_backward_sub(L_L_Z, cached_einsum('lsr,smnS,LSR->lmLrnR', XAX_k[(2, 2)], block_A_k[(2, 2)], XAX_k1[(2, 2)]).reshape(m, m), overwrite_b=True)
             mL_eq = cached_einsum('lsr,smnS,LSR->lmLrnR', XAX_k[(0, 1)], block_A_k[(0, 1)], XAX_k1[(0, 1)]).reshape(m,m)
             T_op = cached_einsum('lsr,smnS,LSR->lmLrnR', XAX_k[(3, 1)], block_A_k[(3, 1)], XAX_k1[(3, 1)]).reshape(m, m)
-            A = (
-                    mL_eq @ (L_Z_inv_L_X * inv_I.reshape(1, -1)) @ mL_eq.T
-                    + cached_einsum('lsr,smnS,LSR->lmLrnR', XAX_k[(0, 0)], block_A_k[(0, 0)],XAX_k1[(0, 0)]).reshape(m, m)
-            )
+            A = cached_einsum('lsr,smnS,LSR->lmLrnR', XAX_k[(0, 0)], block_A_k[(0, 0)],XAX_k1[(0, 0)]).reshape(m, m).__iadd__(mL_eq @ (L_Z_inv_L_X * inv_I.reshape(1, -1)) @ mL_eq.T)
             B = mL_eq @ L_Z_inv_L_X
             C = T_op @ (L_Z_inv_L_X * inv_I.reshape(1, -1)) @ mL_eq.T
-            D = (
-                cached_einsum('lsr,smnS,LSR->lmLrnR', XAX_k[(3, 3)], block_A_k[(3, 3)], XAX_k1[(3, 3)]).reshape(m, m)
-                + T_op @ L_Z_inv_L_X
-            )
+            D = cached_einsum('lsr,smnS,LSR->lmLrnR', XAX_k[(3, 3)], block_A_k[(3, 3)], XAX_k1[(3, 3)]).reshape(m, m).__iadd__(T_op @ L_Z_inv_L_X)
 
             u = (
                     mR_p - mL_eq @ (L_L_Z_inv_mR_c - (L_Z_inv_L_X * inv_I.reshape(1, -1)) @ mR_d)
@@ -288,7 +282,7 @@ def _tt_ipm_newton_step(
     Delta_Z_tt = _tt_symmetrise(tt_reshape(_tt_get_block(2, Delta_tt), (2, 2)), status.op_tol)
     Delta_Y_tt = tt_rank_reduce(_tt_get_block(0, Delta_tt), eps=status.op_tol, rank_weighted_error=True)
     Delta_Y_tt = tt_rank_reduce(tt_sub(Delta_Y_tt, tt_fast_matrix_vec_mul(status.lag_map_y, Delta_Y_tt, status.eps)), eps=status.op_tol, rank_weighted_error=True)
-    Delta_T_tt = tt_rank_reduce(tt_fast_hadammard(ineq_mask, tt_reshape(_tt_get_block(3, Delta_tt), (2, 2)), status.eps), eps=status.op_tol, rank_weighted_error=True) if status.with_ineq else None
+    Delta_T_tt = tt_rank_reduce(tt_fast_hadammard(ineq_mask, tt_reshape(_tt_get_block(3, Delta_tt), (2, 2)), status.eps), eps=0.1*status.op_tol, rank_weighted_error=True) if status.with_ineq else None
 
     x_step_size, z_step_size, permitted_error = _tt_line_search(
         X_tt,
@@ -361,7 +355,7 @@ def _tt_ipm_newton_step(
         Delta_Z_tt = tt_rank_reduce(tt_add(Delta_Z_tt_cc, Delta_Z_tt), eps=status.op_tol)
         if status.with_ineq:
             Delta_T_tt_cc = tt_rank_reduce(tt_fast_hadammard(ineq_mask, tt_reshape(_tt_get_block(3, Delta_tt_cc), (2, 2)), status.eps), eps=status.eps, rank_weighted_error=True)
-            Delta_T_tt = tt_rank_reduce(tt_add(Delta_T_tt_cc, Delta_T_tt), eps=status.op_tol, rank_weighted_error=True)
+            Delta_T_tt = tt_rank_reduce(tt_add(Delta_T_tt_cc, Delta_T_tt), eps=0.1*status.op_tol, rank_weighted_error=True)
 
         x_step_size, z_step_size, permitted_error = _tt_line_search(
             X_tt,
@@ -547,9 +541,7 @@ def tt_ipm(
     # Normalisation
     # We normalise the objective to the scale of the average constraint
     status.primal_error_normalisation = 1 + tt_norm(bias_tt)
-    normalisation = np.divide(np.sqrt(tt_inner_prod(lin_op_tt, lin_op_tt) + status.num_ineq_constraints), 2 ** dim + status.num_ineq_constraints)
-    status.dual_error_normalisation = 1 + normalisation
-    obj_tt = tt_normalise(obj_tt, radius=normalisation)
+    status.dual_error_normalisation = 1 + tt_norm(obj_tt)
 
     # KKT-system prep
     lhs_skeleton = {}
@@ -629,7 +621,7 @@ def tt_ipm(
         X_tt, Z_tt = _update(x_step_size, z_step_size, X_tt, Z_tt, Delta_X_tt, Delta_Z_tt, op_tol, permitted_error)
         Y_tt = tt_rank_reduce(tt_add(Y_tt, tt_scale(z_step_size, Delta_Y_tt)), eps=op_tol, rank_weighted_error=True)
         if status.with_ineq:
-            T_tt = tt_rank_reduce(tt_add(T_tt, tt_scale(z_step_size, Delta_T_tt)), eps=op_tol, rank_weighted_error=True)
+            T_tt = tt_rank_reduce(tt_add(T_tt, tt_scale(z_step_size, Delta_T_tt)), eps=0.1*op_tol, rank_weighted_error=True)
         if status.is_last_iter:
             finishing_steps -= 1
 